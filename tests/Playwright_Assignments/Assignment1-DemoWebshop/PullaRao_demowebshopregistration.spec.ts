7272e90e49d47890b3fee3aa623d5a3355d52f63
sagar-automationcatalogue <sagar.automationcatalogue@gmail.com>
2026-08-24 06:48:19 +0530

    Playwright Locators programs

--- tests/Playwright_Assignments/Assignment1-DemoWebshop/PullaRao_demowebshopregistration.spec.ts
+++ tests/Playwright_Assignments/Assignment1-DemoWebshop/PullaRao_demowebshopregistration.spec.ts
@@ -20,8 +20,23 @@ test(`Tricenties Registration Test Case`, async ({browser})=>{
      await page.waitForTimeout (1000);
      await page.locator(`#register-button`).click();
      await page.waitForTimeout (1000);
+})
 
-     
+test (`Tricentis Login Test Case`, async ({browser})=>{
 
-   
+     const conext = await browser.newContext();
+     const page = await conext.newPage();
+     page.goto (`https://demowebshop.tricentis.com/login`);
+     await page.waitForTimeout(1000);
+     await page.locator (`#Email`).fill(`vxm@gmail.com`);
+     await page.waitForTimeout(1000);
+     await page.locator(`#Password`).fill(`Admin@123`);
+     await page.waitForTimeout(1000);
+     //await page.locator(`.button-1 login-button`).click();
+     await page.locator('.button-1.login-button').click();
+     await page.waitForTimeout(1000);
+     await page.locator(`.ico-logout`).click();
+     await page.waitForTimeout(1000);
+
+
 })
