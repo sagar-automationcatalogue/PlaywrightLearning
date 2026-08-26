8773d98be911d16621056303165243190c85645e
sagar-automationcatalogue <sagar.automationcatalogue@gmail.com>
2026-08-26 07:27:27 +0530

    select drop-down

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',  
   workers: 1,
-  timeout:60_000,  
+  timeout:30_000,  
   use: {
     browserName:'chromium',
     headless: false
