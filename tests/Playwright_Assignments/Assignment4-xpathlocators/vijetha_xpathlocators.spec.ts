e9865d51c52ad6b60b04101c13474ac5dfb58677
vijgodbole <vijethagodbole.2012@gmail.com>
2026-09-01 21:08:51 -0400

    v's xpath

---
+++ tests/Playwright_Assignments/Assignment4-xpathlocators/vijetha_xpathlocators.spec.ts
@@ -0,0 +1,17 @@
+
+import {test} from '@playwright/test';
+
+test(`xpath locators`, async({page})=>{
+    await page.goto(`https://www.automationexercise.com/`);
+
+    await page.locator(`xpath=html/body/header/div/div/div/div[2]/div/ul/li[4]/a`).click();
+    await page.locator(`xpath=html/body/section/div/div/div[1]/div[1]/form[1]/input[2]`).fill(`sagar.automationcatalogue8@gmail.com`);
+    await page.locator(`xpath=html/body/section/div/div/div[1]/div/form[1]/input[3]`).fill(`Admin@123`);
+    await page.locator(`xpath=html/body/section/div/div/div[1]/div/form[1]/button[1]`).click();
+    await page.waitForLoadState(`domcontentloaded`);
+
+    let message= await page.locator(`xpath=html/body/header/div/div/div/div[2]/div/ul/li[10]`).textContent();
+    console.log(message)
+    
+    await page.waitForTimeout(4000);
+})
