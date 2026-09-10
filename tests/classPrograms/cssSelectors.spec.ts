f00a23577c44bbca1d10d3a15bbf24ca5d592b09
sagar-automationcatalogue <sagar.automationcatalogue@gmail.com>
2026-09-10 08:29:35 +0530

    Playwright Assertions

--- tests/classPrograms/cssSelectors.spec.ts
+++ tests/classPrograms/cssSelectors.spec.ts
@@ -7,8 +7,16 @@ test(`CSS Selectors and Playwright Assertions`, async({page}) =>{
     await page.locator(`.password-input`).fill(`Admin@123`);
     await page.locator(`button[type='submit']`).click();
 
-    await expect.soft(page).toHaveURL('dashboard');
+    await expect.soft(page).toHaveURL('https://automation44-trials8101.orangehrmlive.com/client/#/dashboard');
+    //await page.locator(`span[class='menu-title-text tooltipped'][data-tooltip='Leave']`).first().click();
+
+    const profileName_locator = await page.locator(`#sidebar-profile-picture>a`);
+    await expect.soft(profileName_locator).toContainText("Vijetha");
+
+    const logout_locator = await page.locator(`li[id='navbar-logout'] a span`);
+    await expect.soft(logout_locator).toBeVisible();
     await page.locator(`span[class='menu-title-text tooltipped'][data-tooltip='Leave']`).first().click();
+
     await page.waitForTimeout(4000);
 
 })
