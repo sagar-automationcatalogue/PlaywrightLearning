cbed2c460c5f40333eb9a8ddb5aca959b9818ea8
sagar-automationcatalogue <sagar.automationcatalogue@gmail.com>
2026-09-12 07:40:42 +0530

    Playwright timeout concepts

--- tests/classPrograms/latest_orangehrm.login.spec.ts
+++ tests/classPrograms/latest_orangehrm.login.spec.ts
@@ -1,22 +1,26 @@
 import {test} from '@playwright/test';
 
 test(`Orange HRM Login testcase`, async ({browser})=>{
+    test.setTimeout(180_000);
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto(`https://automation44-trials8101.orangehrmlive.com`);
 
+    await page.waitForTimeout(3_000);
+
     const userNameLocator = await page.locator('#txtUsername');
     await userNameLocator.fill('admin');
 
-    await page.waitForTimeout(3000);
+    await page.waitForTimeout(3_000);
 
     await page.locator('#txtPassword').fill(`Admin@123`);
-    await page.waitForTimeout(3000);
+    //await page.waitForTimeout(30_000);
 
-    //await page.locator('.icon.login-icon').click();
+    await page.locator('.icon.login-icon').click();
     //await page.waitForTimeout(5000);
+    
 
-    const message = await page.locator(`.remember-me-field`).textContent();
-    console.log(message?.trim().toUpperCase());
+    //const message = await page.locator(`.remember-me-field`).textContent();
+    //console.log(message?.trim().toUpperCase());
     
 });
